6125c20aedd62b2b3e6286f63a9c9e369ced712a
juan267 <j.gomez267@gmail.com>
2015-12-11 14:26:06 -0500

    index templates and css fixes

---
+++ js/products.js
@@ -0,0 +1,14 @@
+var products = [
+    { "id": 0, "img": "images/pink.jpg", "name": "Streach Knit Wrap (Pink)", "price": "14.95", "description": "Beatifull products for babys. We re the best provider in the world", "height": 10, "width": 10, "category": "Baby", "extraInfo": "Its Awesome", "retail": 1, "discount": 10 },
+    { "id": 1, "img": "images/purple.jpg", "name": "Streach Knit Wrap (Purple)", "price": "14.95", "description": "Beatifull products for babys. We re the best provider in the world", "height": 10, "width": 10, "category": "Baby", "extraInfo": "Its Awesome", "retail": 1, "discount": 10 },
+    { "id": 2, "img": "images/white.jpg", "name": "Streach Knit Wrap (White)", "price": "14.95", "description": "Beatifull products for babys. We re the best provider in the world", "height": 10, "width": 10, "category": "Baby", "extraInfo": "Its Awesome", "retail": 1, "discount": 10 },
+    { "id": 3, "img": "images/blue.jpg", "name": "Streach Knit Wrap (Blue)", "price": "14.95", "description": "Beatifull products for babys. We re the best provider in the world", "height": 10, "width": 10, "category": "Baby", "extraInfo": "Its Awesome", "retail": 1, "discount": 10 },
+    { "id": 4, "img": "images/aqua.jpg", "name": "Streach Knit Wrap (Mint)", "price": "14.95", "description": "Beatifull products for babys. We re the best provider in the world", "height": 10, "width": 10, "category": "Baby", "extraInfo": "Its Awesome", "retail": 1, "discount": 10 },
+    { "id": 5, "img": "images/caddybag.jpg", "name": "CaddyBag", "price": "15", "description": "Beatifull products for babys. We re the best provider in the world", "height": 10, "width": 10, "category": "Baby", "extraInfo": "Its Awesome", "retail": 1, "discount": 10 },
+    { "id": 6, "img": "images/caddybag.jpg", "name": "CaddyBag", "price": "15", "description": "Beatifull products for babys. We re the best provider in the world", "height": 10, "width": 10, "category": "Baby", "extraInfo": "Its Awesome", "retail": 1, "discount": 10 },
+
+    { "id": 7, "img": "images/pacifier.jpg", "name": "Pacifier", "price": "10", "description": "Beatifull products for babys. We re the best provider in the world", "height": 10, "width": 10, "category": "Baby", "extraInfo": "Its Awesome" },
+    { "id": 8, "img": "images/koreanheadband.jpg", "name": "Korean Head Bands for Baby", "extraInfo": "Its Awesome", "price": "10", "description": "Beatifull products for babys. We re the best provider in the world", "height": 10, "width": 10, "category": "Baby", "extraInfo": "Its Awesome" }
+    ];
+
+
