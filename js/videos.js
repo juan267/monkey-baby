30c1b651c14635eebad7d46d97ce523ca987e3dc
juan267 <j.gomez267@gmail.com>
2016-01-03 13:06:55 -0500

    added videso page

---
+++ js/videos.js
@@ -0,0 +1,5 @@
+var videos = [
+  {title: "Basic wrapping technique", url:"http://www.youtube.com/embed/s6KjQ3df1Ek?autoplay=1"},
+  {title: "Newborn wrapping technique, posing with a wrap tutorial:", url:"http://www.youtube.com/embed/2d4F2unmv1U?"},
+  {title: "Basic Posing Techniques:, posing with a wrap tutorial:", url:"http://www.youtube.com/embed/SnR8NQ9VPFU?"},
+]
